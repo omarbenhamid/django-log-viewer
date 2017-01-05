22bcd2e650a16ea763b3413da351d2fca2553a58
Adolfo Tamayo <adolfo@inka-labs.com>
2017-01-05 16:03:06 -0500

    adding setup, README and gitignore

---
+++ README.rst
@@ -0,0 +1,26 @@
+=====
+Log Viewer
+=====
+
+Log Viewer allows you to read log files in the admin page.
+
+Quick start
+-----------
+
+1. Add "log-viewer" to your INSTALLED_APPS setting like this::
+
+    INSTALLED_APPS = [
+        ...
+        'django-log-viewer',
+    ]
+
+2. Include the log-viewer URLconf in your project urls.py like this::
+
+    url(r'^log_viewer/', include('log-viewer.urls')),
+
+3. In your settings file create the following value:
+   LOG_VIEWER_FILES = ['path/to/logfile1', 'path/to/logfile2', ...]
+
+4. Start the development server and visit http://127.0.0.1:8000/admin/log_viewer/
+
+5.
---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-log-viewer',
+    version='0.1',
+    author="InkaLabs",
+    packages=find_packages(),
+    include_package_data=True,
+    license='BSD License',  # example license
+    description='A simple Django app to read log files in the admin.',
+    long_description=README,
+    url='https://bitbucket.org/inkalabsinc/django-log-viewer/',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: X.Y',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'Programming Language :: Python',
+    ],
+)
