482dce74e56b4647682fabd391a6c1d417dbaa70
Omar BENHAMID <contact@obenhamid.me>
2019-10-16 05:43:14 +0200

    Fixes to log viewer

--- log_viewer/urls.py
+++ log_viewer/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name='log_viewer'
+
 urlpatterns = [
     url(r'^(?P<file_name>[\.\w-]*)/(?P<page>[0-9]+)', views.log_viewer,
         name='logfile-view'),
--- log_viewer/utils.py
+++ log_viewer/utils.py
@@ -1,17 +1,22 @@
 import os
+from django.conf import settings
 
 
 def readlines_reverse(qfile, exclude=''):
     """
     Read file lines from bottom to top
     """
+    pfx = getattr(settings, 'LOG_VIEWER_LINE_PREFIX', '')
+    pfx = pfx[::-1]
+    pfxlen = len(pfx)
+    
     qfile.seek(0, os.SEEK_END)
     position = qfile.tell()
     line = ''
     while position >= 0:
         qfile.seek(position)
         next_char = qfile.read(1)
-        if next_char == "\n" and line and line[-1] == '[':
+        if next_char == "\n" and line and (line[-pfxlen:] == pfx if pfxlen else True):
             if exclude in line[::-1]:
                 line = ''
             else:
